af6b0fcf69f8820c7dce08a0e88529bff2983c53
Bishnu Nath <bishnu1324@gmail.com>
2021-09-29 20:32:28 +0600

    25-8 Money withdraw and reduce balance for withdraw

--- js/banking.js
+++ js/banking.js
@@ -22,3 +22,30 @@ document.getElementById('deposit-button').addEventListener
   // clear the deposit input field
   depositInput.value = '';
 })
+
+// handle withdraw button event handler
+document.getElementById('withdraw-button').addEventListener
+('click', function () {
+  const withdrawInput = document.getElementById('withdraw-input');
+  const withdrawAmountText = withdrawInput.value;
+  const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+  // set withdraw total
+  const withdrawTotal = document.getElementById('withdraw-total');
+  const previousWithdrawText = withdrawTotal.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotal.innerText = newWithdrawTotal;
+
+  // update balance
+  const balanceTotal = document.getElementById('balance-total');
+  const previousBalanceText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceText);
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+  balanceTotal.innerText = newBalanceTotal;
+
+  // clear withdraw input
+  withdrawInput.value = '';
+})
